dd3f9351ba2e94a60d55b02eaab3edfbd8581183
Nick Stebb <nickolarse@riseup.net>
2021-02-21 09:59:42 +1300

    Setup postcss

---
+++ src/app.js
@@ -0,0 +1,4 @@
+// Import our CSS
+import "./css/app-base.pcss";
+import "./css/app-components.pcss";
+import "./css/app-utilities.pcss";
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/app.js",
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "build.js",
+  },
+  plugins: [
+    new MiniCssExtractPlugin({ filename: "bundle.css" }),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: ["html-loader"],
+      },
+      {
+        test: /\.(sc|c|pc)ss$/,
+        exclude: /node_modules/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader", // translates CSS into CommonJS modules
+          },
+          {
+            loader: "postcss-loader", // Run post css actions
+            options: {
+              postcssOptions: {
+                config: path.resolve(__dirname, "postcss.config.js"),
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  watch: true,
+};
