47819592f3b2463701703919252ea3b36a91fa2f
Nick Stebb <nickolarse@riseup.net>
2021-02-22 19:19:54 +1300

    Fix responsive navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,32 @@ module.exports = {
       cover: "cover",
       contain: "contain",
       "50%": "50%",
+      "40%": "40%",
+    },
+    colors: {
+      balance: {
+        alert: "#F25042",
+        success: "#80E055",
+        info: "#56CCF2",
+        darkprimary: "#CFDC4F",
+        lightprimary: "#E8F18F",
+        darkbg: "#50442E",
+        lightbg: "#8C7851",
+        darkgray: "#E2E1E1",
+        lightgray: "#F1EDEB",
+        lilac: "#EDD3F8",
+        DEFAULT: "#121428",
+      },
     },
     // linearGradientColors: (theme) => theme("colors"),
     // radialGradientColors: (theme) => theme("colors"),
     // conicGradientColors: (theme) => theme("colors"),
     // Extend the default Tailwind config here
-    extend: {},
+    extend: {
+      outline: {
+        light: "2px solid #0000ff",
+      },
+    },
     // Replace the default Tailwind config here
   },
   corePlugins: {},
