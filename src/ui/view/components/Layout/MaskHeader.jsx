58cd41a5f74e2aaffadd1ffe054384503f8a23f5
Nick Stebb <nickolarse@riseup.net>
2021-03-13 17:55:58 +1300

    Make responsive nav buttons bigger on sm screens

--- src/ui/view/components/Layout/MaskHeader.jsx
+++ src/ui/view/components/Layout/MaskHeader.jsx
@@ -8,12 +8,15 @@ import "../../../assets/css/components/partials/MaskHeader.scss";
 const MaskHeader = {
   view: ({ attrs, children }) => (
     <div class="mask-wrapper">
-      <header class="bg-balance-dp z-10 flex-none h-12">
-        <div id="responsive-nav" class="flex items-start justify-between">
-          <a class="logo block w-8 h-8">
+      <header class="bg-balance-dp z-10 flex-none h-16 md:h-12">
+        <div
+          id="responsive-nav"
+          class="flex h-16 md:h-10 px-4 md:px-2 lg:px-0 items-center justify-between"
+        >
+          <a class="logo block h-10 md:h-8">
             <svg
               id="logo"
-              class="hover:text-balance-lmint md:ml-1 w-8 h-8 text-gray-100 fill-current stroke-current"
+              class="hover:text-balance-lmint md:ml-1 w-10 md:w-8 h-10 md:h-8 text-gray-100 fill-current stroke-current"
               width="54"
               height="54"
               viewBox="0 0 54 54"
@@ -22,11 +25,11 @@ const MaskHeader = {
               <path d="M15.34,45.5C8.26,45.5,2.5,39.74,2.5,32.66s5.76-12.84,12.84-12.84h1.31v8.37l-1.31,0c-2.47,0-4.47,2.01-4.47,4.47  c0,2.47,2.01,4.48,4.47,4.48c2.47,0,4.47-2.01,4.47-4.48l0-1.92v-15.4c0-7.08,5.76-12.84,12.84-12.84c7.08,0,12.84,5.76,12.84,12.84  s-5.76,12.84-12.84,12.84h-1.31v-8.37l1.31,0c2.47,0,4.47-2.01,4.47-4.47c0-2.47-2.01-4.47-4.47-4.47c-2.47,0-4.47,2.01-4.47,4.47  l0,1.92v15.4C28.18,39.74,22.42,45.5,15.34,45.5z" />
             </svg>
           </a>
-          <div id="hamburger-wrapper" class="lg:h-8 lg:flex-auto flex">
-            <div class="active:outline-light focus:outline-light hover:bg-balance-lp flex items-center justify-center w-8 h-8 rounded-md">
+          <div id="hamburger-wrapper" class="h-10 md:h-8 lg:flex-auto flex">
+            <div class="active:outline-light focus:outline-light hover:bg-balance-lp flex items-center justify-center h-10 md:w-8 w-10 md:h-8 rounded-md">
               <label
                 for="hamburger"
-                class="text-balance-secondary border-1 lg:hidden block w-6 h-6"
+                class="text-balance-secondary border-1 lg:hidden block w-8 md:w-6 h-8 md:h-6"
               >
                 <svg
                   class="text-balance-secondary stroke-current"
